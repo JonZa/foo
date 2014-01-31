aa6b42d51780b300e37007057732e37fe0890faa
JonZa <jon.muller@gmail.com>
2014-01-31 18:18:23 +0200

    Null check
    Crazy that we got this far without it

--- foo.js
+++ foo.js
@@ -1,14 +1,19 @@
 /*------------------------------------------------------- foo(console.log,shorthand) */
 function foo(bar, fly) {
-	var paranoid = false;
+	var paranoid = true;
 	if (!window.console || (fly && !paranoid)) {
 		return false;
-	} else if (bar === undefined) {
+	} else if (bar === undefined || bar === null) {
 		bar = 'o_O';
 	}
-	if (typeof bar ==="object") {
-		console.log('[' + new Date().toUTCString().split(' ')[4] + '] object:');
-		console.dir(bar);
+	if (typeof bar ==='object') {
+		if (bar.jquery != undefined) {
+			console.log('[' + new Date().toUTCString().split(' ')[4] + '] jquery object:');
+			console.log(bar);
+		} else {
+			console.log('[' + new Date().toUTCString().split(' ')[4] + '] object:');
+			console.dir(bar);
+		}
 	} else {
 		console.log('[' + new Date().toUTCString().split(' ')[4] + '] ' + bar);
 	}
